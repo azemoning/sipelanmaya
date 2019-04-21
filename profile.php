3ecdb3bf84d8741bdae69fe18b1b00631db2d5a1
azemoning <ahmadsaufimaulana@gmail.com>
2019-04-21 17:08:04 +0700

    change css href

--- profile.php
+++ profile.php
@@ -6,13 +6,13 @@
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="css/profile.css">
 <title>SIPELAN MAYA - Profile</title>
 </head>
 <body>
   <form class="box" action="../MainPage/index.html" method="post">
     <div class="logoBox">
-      <img src="avatar.png" alt="SIPELAN MAYA"></img>
+      <img src="avatar.png" alt="SIPELAN MAYA">
     </div>
     <input type="email" name="" placeholder="user@example.com" disabled>
     <input type="text" name="" placeholder="DISPLAY NAME">
