beeedfd95773d6652a3a0118cd0e9ce0eda1c6a5
azemoning <ahmadsaufimaulana@gmail.com>
2019-04-21 16:16:02 +0700

    remove OR email clause query on login.php

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ if(isset($_POST['login'])){
     $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
     $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
 
-    $sql = "SELECT * FROM users WHERE username=:username OR email=:email";
+    $sql = "SELECT * FROM users WHERE username=:username";
     $stmt = $db->prepare($sql);
     
     // bind parameter ke query
