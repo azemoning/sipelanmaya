cd490b6b9df4b0598faaec072b906c86015efa0b
azemoning <ahmadsaufimaulana@gmail.com>
2019-04-21 23:07:10 +0700

    change database connection method

--- config.php
+++ config.php
@@ -1,18 +1,18 @@
 <?php
 
-$db_host = "sipelanmaya.mysql.database.azure.com";
-$db_user = "sipelanmaya@sipelanmaya";
-$db_pass = "@Nyb291lu23";
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
 $db_name = "sipm_db";
 
-// try {    
-//     //create PDO connection 
-//     $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
-// } catch(PDOException $e) {
-//     //show error
-//     die("Terjadi masalah: " . $e->getMessage());
-// }
+ try {    
+     //create PDO connection 
+     $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+ } catch(PDOException $e) {
+     //show error
+     die("Terjadi masalah: " . $e->getMessage());
+ }
 
-$con=mysqli_init(); 
-//mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); 
-mysqli_real_connect($con, $db_host, $db_user, $db_pass, $db_name, 3306);
+// $con=mysqli_init(); 
+// mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); 
+// mysqli_real_connect($con, $db_host, $db_user, $db_pass, $db_name, 3306);
