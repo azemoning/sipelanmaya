cf65825820444ef3d926f9ed106c8b99246e0908
azemoning <ahmadsaufimaulana@gmail.com>
2019-04-21 20:57:33 +0700

    change mysql connect method

--- config.php
+++ config.php
@@ -5,10 +5,14 @@ $db_user = "sipelanmaya@sipelanmaya";
 $db_pass = "@Nyb291lu23";
 $db_name = "sipm_db";
 
-try {    
-    //create PDO connection 
-    $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
-} catch(PDOException $e) {
-    //show error
-    die("Terjadi masalah: " . $e->getMessage());
-}
+// try {    
+//     //create PDO connection 
+//     $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+// } catch(PDOException $e) {
+//     //show error
+//     die("Terjadi masalah: " . $e->getMessage());
+// }
+
+$con=mysqli_init(); 
+//mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); 
+mysqli_real_connect($con, $db_host, $db_user, $db_pass, $db_name, 3306);
