6e144110c641bef67432f52a0ef88c5b3271cd4c
azemoning <ahmadsaufimaulana@gmail.com>
2019-04-21 17:06:05 +0700

    add html script on profile.php

---
+++ profile.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+<link rel="stylesheet" href="style.css">
+<title>SIPELAN MAYA - Profile</title>
+</head>
+<body>
+  <form class="box" action="../MainPage/index.html" method="post">
+    <div class="logoBox">
+      <img src="avatar.png" alt="SIPELAN MAYA"></img>
+    </div>
+    <input type="email" name="" placeholder="user@example.com" disabled>
+    <input type="text" name="" placeholder="DISPLAY NAME">
+    <input type="password" name="" placeholder="NEW PASSWORD">
+    <button type="submit">SAVE</button>
+  </form>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
