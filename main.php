5895520b7d116740579a3695136e2e9bbde80be3
azemoning <ahmadsaufimaulana@gmail.com>
2019-04-21 16:31:34 +0700

    remove form method for logout modal on main.php

--- main.php
+++ main.php
@@ -202,7 +202,7 @@
                 <div class="modal-body">
                     <p>Are you sure you want to logout?</p>
                     <div class="actionsBtns">
-                        <form action="../LoginPage/index.html" method="post">
+                        <form action="" method="post">
                             <input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}" />
                             <input type="submit" class="btn btn-default btn-primary" data-dismiss="modal" value="Logout"
                                 onclick="window.location='../LoginPage/index.html'" />
