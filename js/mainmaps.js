e17bff2fcf53b1e72c91fa80fa90f188065da061
azemoning <ahmadsaufimaulana@gmail.com>
2019-04-23 11:38:24 +0700

    commenting bugged line

--- js/mainmaps.js
+++ js/mainmaps.js
@@ -58,39 +58,39 @@ function initMap() {
             map.setCenter(place.geometry.location);
             map.setZoom(17);
         }
-        marker.setIcon(({
-            url: place.icon,
-            size: new google.maps.Size(71, 71),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(17, 34),
-            scaledSize: new google.maps.Size(35, 35)
-        }));
-        marker.setPosition(place.geometry.location);
-        marker.setVisible(true);
+        // marker.setIcon(({
+        //     url: place.icon,
+        //     size: new google.maps.Size(71, 71),
+        //     origin: new google.maps.Point(0, 0),
+        //     anchor: new google.maps.Point(17, 34),
+        //     scaledSize: new google.maps.Size(35, 35)
+        // }));
+        // marker.setPosition(place.geometry.location);
+        // marker.setVisible(true);
 
-        var address = '';
-        if (place.address_components) {
-            address = [
-                (place.address_components[0] && place.address_components[0].short_name || ''),
-                (place.address_components[1] && place.address_components[1].short_name || ''),
-                (place.address_components[2] && place.address_components[2].short_name || '')
-            ].join(' ');
-        }
+        // var address = '';
+        // if (place.address_components) {
+        //     address = [
+        //         (place.address_components[0] && place.address_components[0].short_name || ''),
+        //         (place.address_components[1] && place.address_components[1].short_name || ''),
+        //         (place.address_components[2] && place.address_components[2].short_name || '')
+        //     ].join(' ');
+        // }
 
-        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-        infowindow.open(map, marker);
+        // infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+        // infowindow.open(map, marker);
 
-        //Location details
-        for (var i = 0; i < place.address_components.length; i++) {
-            if (place.address_components[i].types[0] == 'postal_code') {
-                document.getElementById('postal_code').innerHTML = place.address_components[i].long_name;
-            }
-            if (place.address_components[i].types[0] == 'country') {
-                document.getElementById('country').innerHTML = place.address_components[i].long_name;
-            }
-        }
-        document.getElementById('location').innerHTML = place.formatted_address;
-        document.getElementById('lat').innerHTML = place.geometry.location.lat();
-        document.getElementById('lon').innerHTML = place.geometry.location.lng();
+        // //Location details
+        // for (var i = 0; i < place.address_components.length; i++) {
+        //     if (place.address_components[i].types[0] == 'postal_code') {
+        //         document.getElementById('postal_code').innerHTML = place.address_components[i].long_name;
+        //     }
+        //     if (place.address_components[i].types[0] == 'country') {
+        //         document.getElementById('country').innerHTML = place.address_components[i].long_name;
+        //     }
+        // }
+        // document.getElementById('location').innerHTML = place.formatted_address;
+        // document.getElementById('lat').innerHTML = place.geometry.location.lat();
+        // document.getElementById('lon').innerHTML = place.geometry.location.lng();
     });
 }
