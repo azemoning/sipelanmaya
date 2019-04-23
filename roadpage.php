8393c0c129ad1109ff370c1d7489fc13c082cfea
azemoning <ahmadsaufimaulana@gmail.com>
2019-04-23 20:12:02 +0700

    add insert new locations feature php script

--- roadpage.php
+++ roadpage.php
@@ -1,3 +1,40 @@
+<?php
+    require_once("config.php");
+    session_start();
+    if(isset($_POST['save'])){
+    
+    // tentukan lokasi file akan dipindahkan
+    $dirUpload = "img/user/";
+    $imagename = $_FILES["gambar"]["name"];
+    $target_file = $dirUpload . $imagename;
+    // pindahkan file
+    $terupload = move_uploaded_file($_FILES["gambar"]["tmp_name"], $target_file);
+
+    // filter data yang diinputkan
+    $lat = $_POST['lat'];
+    $lng = $_POST['lng'];
+    $label = filter_input(INPUT_POST, 'label', FILTER_SANITIZE_STRING);
+    $description = filter_input(INPUT_POST, 'description', FILTER_SANITIZE_STRING);
+    $media = $imagename;
+    $id = $_SESSION["id"];
+
+    $db = new dbconnect;
+    $db = $db->connect();
+    $stmt = $db->prepare("INSERT INTO markers VALUES (:id, NULL, :label, :description, :lat, :lng, :media)");
+    $params = array(
+        ":id" => $id,
+        ":lat" => $lat,
+        ":lng" => $lng,
+        ":label" => $label,
+        ":description" => $description,
+        ":media" => $media
+    
+    );
+    $insertLocation = $stmt->execute($params);
+    if($insertLocation) header("Location: main.php");
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,19 +44,21 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
 <link rel="stylesheet" href="css/roadpage.css">
-<link href="https://unpkg.com/filepond/dist/filepond.css" rel="stylesheet">
+<!-- <link href="https://unpkg.com/filepond/dist/filepond.css" rel="stylesheet">
 <link href="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css" rel="stylesheet">
-<script src="https://unpkg.com/filepond/dist/filepond.js"></script>
+<script src="https://unpkg.com/filepond/dist/filepond.js"></script> -->
 
 <title>SIPELAN MAYA - Road Page</title>
 </head>
 <body>
 <div id="map"></div>
 <div class="dataPage">
-    <form action="" class="dataForm" method="POST" enctype="multipart/form-data">
+    <form action="" class="dataForm" method="post" enctype="multipart/form-data">
+    <input type="hidden" name="lat" id="lat">
+    <input type="hidden" name="lng" id="lng">
     <div class="imageHolder form-group">
         <img src="img/page/jembatan.jpg">
-        <input type="file" id="fileInput">
+        <input type="file" id="gambar" name="gambar">
     </div>
     <div class="descriptionContent">
     <div class="row">
@@ -43,6 +82,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
-<script src="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.js"></script>
+<!-- <script src="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.js"></script> -->
 </body>
 </html>
