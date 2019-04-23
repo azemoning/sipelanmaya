c9aa9dee6417e9c773e7eae4811f6545b242f755
azemoning <ahmadsaufimaulana@gmail.com>
2019-04-23 14:50:53 +0700

    redesign php script structure with class

--- config.php
+++ config.php
@@ -1,18 +1,24 @@
 <?php
+class dbconnect{
+        private $db_host = "localhost";
+        private $db_user = "root";
+        private $db_pass = "";
+        private $db_name = "sipm_db";
+        
+        public function connect(){
+            try {    
+                //create PDO connection 
+                $db = new PDO('mysql:host=' . $this->db_host . ';dbname=' . $this->db_name, $this->db_user, $this->db_pass);
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                return $db;
+            } catch(PDOException $e) {
+                //show error
+                die("Terjadi masalah: " . $e->getMessage());
+            }
+        }
 
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "sipm_db";
-
- try {    
-     //create PDO connection 
-     $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
- } catch(PDOException $e) {
-     //show error
-     die("Terjadi masalah: " . $e->getMessage());
- }
 
 // $con=mysqli_init(); 
 // mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); 
 // mysqli_real_connect($con, $db_host, $db_user, $db_pass, $db_name, 3306);
+}
