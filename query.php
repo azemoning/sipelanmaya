0ba983b10707ebe9c7675564514693dcf5b3e4de
azemoning <ahmadsaufimaulana@gmail.com>
2019-04-23 14:51:18 +0700

    add query.php file for custom mysql query

---
+++ query.php
@@ -0,0 +1,41 @@
+<?php
+    class query{
+        private $id;
+        private $id_location;
+        private $label;
+        private $description;
+        private $lat;
+        private $lng;
+        private $media;
+
+        function setId($id) { $this->id = $id; }
+        function getId() { return $this->id; }
+        function setId_location($id_location) { $this->id_location = $id_location; }
+        function getId_location() { return $this->id_location; }
+        function setLabel($label) { $this->label = $label; }
+        function getLabel() { return $this->label; }
+        function setDescription($description) { $this->description = $description; }
+        function getDescription() { return $this->description; }
+        function setLat($lat) { $this->lat = $lat; }
+        function getLat() { return $this->lat; }
+        function setLng($lng) { $this->lng = $lng; }
+        function getLng() { return $this->lng; }
+        function setMedia($media) { $this->media = $media; }
+        function getMedia() { return $this->media; }
+
+        public function __construct()
+        {
+            require_once("config.php");
+            $conn = new dbconnect;
+            $this->conn = $conn->connect();
+        }
+
+        public function getAllLocations() {
+            $sql = "SELECT markers.*,users.name FROM markers LEFT JOIN users ON markers.id=users.id";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+    }
+?>
